ed4561b099cb69775b79a34dd5f173b70b48edc8
funes781 <121256208+funes781@users.noreply.github.com>
2023-09-17 17:30:09 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+--[[ FX Information ]]--
+fx_version   'cerulean'
+lua54        'yes'
+game         'gta5'
+
+--[[ Resource Information ]]--
+name         'fun-picklock'
+author       'funes781'
+version      '2.1.6'
+description  'ESX picklock script'
+
+--[[ Manifest ]]--
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+
+
